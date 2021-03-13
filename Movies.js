3dc9983c233188f5d8b9ca58fb709d4bb6230ee8
Josh Mansito <joshua.mansito@ucdenver.edu>
2021-03-13 13:16:35 -0700

    Updated /movies routes

---
+++ Movies.js
@@ -0,0 +1,32 @@
+let envPath = __dirname + "/../.env"
+require('dotenv').config({path:envPath});
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+mongoose.Promise= global.Promise;
+
+try {
+    mongoose.connect( process.env.DB, {useNewUrlParser: true, useUnifiedTopology: true}, () =>
+        //check for connection
+        console.log("connected to movies"));
+}catch (error) {
+    console.log("could not connect to movies");
+}
+mongoose.set('useCreateIndex', true);
+
+//Making movie schema with requirements for each parameter
+var MovieSchema = new Schema({
+
+    //Title of movie, string, must be a unique entry to avoid repeats
+    title:{type:String,required:true,index:{unique:true}},
+
+    //Year of the movie accepts a date
+    year_Released:{type:Date, required:true},
+
+    //Genre follows the requirements of the this list. May change to accept a string instead of this array in case there are other categories not on this list
+    genre:{type:String,required:true,enum:['Action', 'Adventure', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Mystery', 'Thriller', 'Western'] },
+
+    //actors takes two string arrays for the actor name and the character name.
+    actors: { type: [{actorName: String, characterName: String}], required: true }
+});
+module.exports = mongoose.model('Movie',MovieSchema);
--- server.js
+++ server.js
@@ -4,6 +4,8 @@ File: Server.js
 Description: Web API scaffolding for Movie API
  */
 
+let envPath = __dirname + "/.env"
+require('dotenv').config({path:envPath});
 var express = require('express');
 var bodyParser = require('body-parser');
 var passport = require('passport');
@@ -21,6 +23,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(passport.initialize());
 
 var router = express.Router();
+var Movies = require('./movies');
 
 function getJSONObjectForMovieRequirement(req) {
     var json = {
@@ -62,64 +65,128 @@ router.post('/signup', function(req, res) {
     }
 });
 
-router.post('/signin', function (req, res) {
+router.post('/signin', function(req, res) {
     var userNew = new User();
+    userNew.name = req.body.name;
     userNew.username = req.body.username;
     userNew.password = req.body.password;
 
     User.findOne({ username: userNew.username }).select('name username password').exec(function(err, user) {
-        if (err) {
-            res.send(err);
-        }
+        if (err) res.send(err);
 
-        user.comparePassword(userNew.password, function(isMatch) {
+        user.comparePassword(userNew.password, function(isMatch){
             if (isMatch) {
-                var userToken = { id: user.id, username: user.username };
+                var userToken = {id: user._id, username: user.username};
                 var token = jwt.sign(userToken, process.env.SECRET_KEY);
-                res.json ({success: true, token: 'JWT ' + token});
+                res.json({success: true, token: 'JWT ' + token});
             }
             else {
-                res.status(401).send({success: false, msg: 'Authentication failed.'});
+                res.status(401).send({success: false, message: 'Authentication failed.'});
             }
-        })
-    })
+        });
+    });
 });
-let db;
+
+//routing for movies
 
 router.route('/movies')
-    .get(function (req, res){
-        console.log(req.body)
-        db.collection('Movies').find({}).toArray((err, data)=>{
-            if(err)throw err;
-            res.status(200).json(data);
-        });
-        res.status(200).send({success: true, msg: 'GET movies'});
-
-    })
-    .post(function (req, res){
-        res.status(200).send({success: true, msg: 'movie saved'});
-    })
-    .delete(authController.isAuthenticated, function(req, res) {
-            console.log(req.body);
-            res = res.status(200);
-            if (req.get('Content-Type')) {
-                res = res.type(req.get('Content-Type'));
-                res.status(200).send({success: true, msg: 'movie deleted'});
-            }
-            var o = getJSONObjectForMovieRequirement(req);
-            res.json(o);
-        }
-    )
-    .put(authJwtController.isAuthenticated, function(req, res) {
+    //Post route to enter in and save a movie
+    .post(authJwtController.isAuthenticated, function (req, res) {
         console.log(req.body);
-        res = res.status(200);
-        if (req.get('Content-Type')) {
-            res = res.type(req.get('Content-Type'));
-            res.status(200).send({success: true, msg: 'movie updated'});
-        }
-        var o = getJSONObjectForMovieRequirement(req);
-        res.json(o);
+        const movie = new Movies();
+        movie.title = req.body.title;
+        movie.year_Released = req.body.year_Released;
+        movie.genre = req.body.genre;
+        movie.actors = req.body.actors;
+
+        //check if movies exist, maybe error or it has <3 errors.
+        Movies.findOne({title: req.body.title}, function(err, found){
+            //Throw error if error
+            if(err){res.json({message: "Read error \n", error: err});}
+            //if found then throw error, no repeats
+            else if(found){res.json({message: "The movie you entered is already in the database\n"});}
+            //Error if three actors are not entered per requirements
+            else if (movie.actors.length < 3){res.json({message: "Please enter in 3 actors\n"});}
+            else{
+                movie.save(function (err) {
+                    if(err){res.json({message: "Please double check your entry, something was not entered correctly.\n", error: err});}
+
+                    //Else, enter in the movie
+                    else{res.json({message: "The movie has been saved to the database.\n"});}
+                })
+            }
+        });
+    })
+
+    //GET route to find all movies
+    .get(authJwtController.isAuthenticated, function (req, res) {
+        Movies.find(function (err, movie) {
+            if(err) res.json({message: "Read Error. Sorry, please try again. \n", error: err});
+            res.json(movie);
+        })
+    })
+
+    //DELETE route for removing a movie
+    .delete(authJwtController.isAuthenticated, function (req, res){
+        Movies.findOneAndDelete({title: req.body.title}, function (err, movie) {
+
+            if (err) {res.status(400).json({message: "Read error. Sorry, please try again.\n", msg: err})}
+
+            //If NULL then the movie is not there
+            else if(movie == null) {res.json({msg : "The movie is not found"})}
+
+            //Else, delete movie
+            else
+                res.json({msg :"Movie is deleted"})
+        })
     });
+
+//Next GET route for finding a movie by it's id
+router.route('/movies/:movieid')
+
+    //Required authentication for movie id.
+    .get(authJwtController.isAuthenticated, function (req, res) {
+        const id = req.params.movieid;
+
+        //doing a findById here to check the id. If found then send the user the movie, else, throw the error
+        Movies.findById(id, function (err, movie) {
+            if (err) res.send(err);
+            res.json(movie);
+        })
+    });
+
+//New route for PUT. Updating a movie
+router.route('/movies/:id')
+
+    //Required authentication for movie id
+    .put(authJwtController.isAuthenticated, function (req, res) {
+        const conditions = {_id: req.params.id};
+        Movies.findOne({title: req.body.title}, function(err, found) {
+
+            if (err) {res.json({message: "Read error, Please try again \n", error: err});}
+
+            else if(found){res.json({message: "The movie you entered is already in the database\n"});}
+
+            else {
+                //using updateOne here
+                Movies.updateOne(conditions, req.body)
+                    .then(mov => {
+                        if (!mov) {
+                            return res.status(404).end();
+                        }
+                        return res.status(200).json({msg: "Movie is updated"})
+                    })
+                    .catch(err => console.log(err))
+            }
+        })
+    });
+
+//All other routes and methods will throw error
+router.all('*', function(req, res) {
+    res.json({
+        error: 'Your HTTP method is not supported and requires a fix.'
+    });
+});
 
 app.use('/', router);
 app.listen(process.env.PORT || 8080);
